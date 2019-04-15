95eee5e38654012587bbb4279e153d50bbc09a71
ksj1026 <kkjiny12@gmail.com>
2019-04-15 15:49:12 +0900

    1-8 Redirect

--- projects/example1/src/App.js
+++ projects/example1/src/App.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import "./App.css";
 
 const Links = () => (
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/menu">Menu</Link>
+    <Link to="/old">Old</Link>
+    <Link to="/new">New</Link>
   </nav>
-)
+);
 
 const Home = () => <h1>Home</h1>;
-
-const Menu = () => (
-  <div>
-    <h1>Menu</h1>
-    <nav>
-      <Link to='/menu/food'>Food</Link>
-      <Link to='/menu/drinks'>Dirnks</Link>
-      <Link to='/menu/Sides'>Sides</Link>
-    </nav>
-    <div>
-      <Switch>
-        <Route exact path="/menu/food" render={({ match }) => <h1>food</h1>} />
-        <Route exact path="/menu/drinks" render={({ match }) => <h1>drinks</h1>} />
-        <Route exact path="/menu/sides" render={({ match }) => <h1>sides</h1>} />
-        <Route render={({ match }) => <h1>not found</h1>} />
-      </Switch>
-    </div>
-  </div>
-)
 
 const App = () => (
   <Router>
     <div>
-      <Links/>
+      <Links />
       <Route exact path="/" component={Home} />
-      <Route path="/menu" component={Menu} />
+      <Route
+        path="/new/:id"
+        render={({ match }) => <h1>New {match.params.id}</h1>}
+      />
+      <Route
+        path="/old/:id"
+        render={({ match }) => (
+          <div>
+            <Redirect to={"/new/" + match.params.id} />
+          </div>
+        )}
+      />
     </div>
   </Router>
 );
