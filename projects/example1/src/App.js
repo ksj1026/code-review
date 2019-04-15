e7eb73f4d3b2ddcb7087f47674a3f677a23e011b
ksj1026 <kkjiny12@gmail.com>
2019-04-15 15:08:00 +0900

    example 1-7 write <Menu> component

--- projects/example1/src/App.js
+++ projects/example1/src/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import './App.css';
 
 const Links = () => (
   <nav>
-    <Link to="/?id=123">Inline</Link>
-    <Link to={{ pathname: '/', search: 'id'}}>Object</Link>
+    <Link to="/">Home</Link>
+    <Link to="/menu">Menu</Link>
   </nav>
 )
+
+const Home = () => <h1>Home</h1>;
+
+const Menu = () => (
+  <div>
+    <h1>Menu</h1>
+    <nav>
+      <Link to='/menu/food'>Food</Link>
+      <Link to='/menu/drinks'>Dirnks</Link>
+      <Link to='/menu/Sides'>Sides</Link>
+    </nav>
+    <div>
+      <Switch>
+        <Route exact path="/menu/food" render={({ match }) => <h1>food</h1>} />
+        <Route exact path="/menu/drinks" render={({ match }) => <h1>drinks</h1>} />
+        <Route exact path="/menu/sides" render={({ match }) => <h1>sides</h1>} />
+        <Route render={({ match }) => <h1>not found</h1>} />
+      </Switch>
+    </div>
+  </div>
+)
+
 const App = () => (
   <Router>
     <div>
       <Links/>
-      <Route
-        path="/"
-        render={({ match, location }) => (
-          <div>
-            <p>root</p>
-            <div>{JSON.stringify(match)}</div>
-            <div>{JSON.stringify(location)}</div>
-            <div>여기에 매치되는 쿼리 문자열의 id를 출력하세요.</div>
-          </div>
-        )}
-        />
+      <Route exact path="/" component={Home} />
+      <Route path="/menu" component={Menu} />
     </div>
   </Router>
 );
